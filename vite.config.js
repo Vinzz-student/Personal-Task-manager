98f8b1f19cb348df9577374dcb4943912f76c635
Vinzz-student <reynandoandrealviano@gmail.com>
2026-02-23 08:08:07 +0700

    deploy via docs

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Personal-Task-Manager/',
-
-  
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
+  base: '/Personal-Task-Manager/',
+  build: {
+    outDir: 'docs'
   }
 })
